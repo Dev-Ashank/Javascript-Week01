c77243301ea13ed9c83b40a12a9ba3c3713e465d
Dev-ashank <kumardivyashankgaurav@gmail.com>
2023-06-15 10:58:35 +0530

    counter using settimeout

---
+++ 02-async-js/easy/2-counter.js
@@ -0,0 +1,19 @@
+/* ## Counter without setInterval
+
+Without using setInterval, try to code a counter in Javascript. 
+There is a hint at the bottom of the file if you get stuck.
+
+ */
+
+function increaseCounter() {
+  let counter = 0;
+  function increment() {
+    {
+      counter++;
+      console.log(`counter : ${counter}`);
+      setTimeout(increment, 1000);
+    }
+  }
+  increment();
+}
+increaseCounter();
